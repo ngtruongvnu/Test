e4e93e9bcdb1d7532b249f3e38ecce3bde946397
T <th0375187055@gmail.com>
2021-04-30 21:20:09 +0700

    add new file

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+class Box{
+    int l, b, h;
+  public:
+    Box(): l(0), b(0), h(0){};
+    Box(int l,int b,int h): l(l), b(b), h(h){};
+    Box(Box &x): l(x.l), b(x.b), h(x.h){};
+    int getLength() { return l; }
+    int getBreadth(){ return b; }
+    int getHeight() { return h; }
+    long long CalculateVolume(){ return (long long)l*b*h; }
+    bool operator<(const Box& x) {
+        if (l < x.l) return true;
+        if (b < x.b && l == x.l) return true;
+        if (h < x.h && l == x.l && b == x.b) return true;
+        return false;
+    }
+    friend ostream& operator<< (ostream& o, const Box& x){
+        o << x.l << " " << x.b << " " << x.h;
+        return o;
+    }
+};
